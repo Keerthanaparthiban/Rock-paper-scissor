b031e870e9886a926da0148853bb1157488ad75f
Keerthanaparthiban <92458543+Keerthanaparthiban@users.noreply.github.com>
2023-01-18 23:34:37 +0530

    match fadeIn fadeOut js

---
+++ main.js
@@ -0,0 +1,22 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    // start the game
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener("click", () => {
+            introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
+        });
+    };
+
+    // calling the inner fn
+    startGame();
+};
+
+// calling the game fn
+game();
